5289a854a7ddd86238ad4d47b6c4e284f87c6f95
Eduardoblara <eduardo_lara@hotmail.com.br>
2015-09-01 22:14:28 -0300

    ElPodron

---
+++ src/modelo/Login.java
@@ -0,0 +1,27 @@
+package modelo;
+
+
+public class Login {
+    private String nome;
+    private String senha;
+    public String getNome() {
+
+    return nome;
+
+}
+    public String getSenha() {
+
+    return senha;
+
+}
+    public void setNome(String email) {
+
+    this.nome = email;
+
+}
+    public void setSenha(String email) {
+
+this.senha = email;
+
+}
+}
